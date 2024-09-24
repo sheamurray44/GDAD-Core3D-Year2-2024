ac5311e49a5b4dd2ca5272a9402c4322695377b0
Shea Murray <Murray-S85@ulster.ac.uk>
2024-09-24 15:35:11 +0100

    DoneButNotWorking

---
+++ Assets/Game1-Collectathon/Scripts/GameManager_Collectathon.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using TMPro;
+
+public class GameManager_Collectathon : MonoBehaviour
+{
+    public static GameManager_Collectathon instance;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI timerText;
+    public GameObject winText;
+    public GameObject gameOverText;
+
+    private int score = 0;
+    private int totalCollectibles;
+    private float timer = 120f; //2mins
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        totalCollectibles = FindObjectsOfType<Collectible>().Length;
+        UpdateScoreText();
+        winText.SetActive(false);
+        gameOverText.SetActive(false);
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        UpdateTimerText();
+
+        if (timer <= 0)
+        {
+            GameOver();
+        }
+
+        if (score >= totalCollectibles)
+        {
+            Win();
+        }
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+        UpdateScoreText();
+    }
+
+    void UpdateScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    void UpdateTimerText()
+    {
+        timerText.text ="Time: " + Mathf.Ceil(timer).ToString();
+    }
+
+    void Win()
+    {
+        winText.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    void GameOver()
+    {
+        gameOverText.SetActive(true);
+        Time.timeScale = 0f;
+    }
+}
